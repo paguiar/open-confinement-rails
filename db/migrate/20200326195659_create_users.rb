856444eef1d39434fd3c557ce625a846cd1fd045
Pedro Aguiar <paguiar32@gmail.com>
2020-03-26 20:57:15 +0100

    `bundle exec rails generate scaffold User email:string name:string address:string phone:string latitude:float longitude:float`

---
+++ db/migrate/20200326195659_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.string :address
+      t.string :phone
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
